a14f6714692b87e47d7efe8c5dfa848094590989
Ved Patel <ved09003@gmail.com>
2025-08-08 12:21:13 +0530

    renamed bookmarks to paraxis

--- paraxis/urls.py
+++ paraxis/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for bookmarks project.
+URL configuration for paraxis project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
